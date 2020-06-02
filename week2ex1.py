c832da21954b198ecd5c0dde033fb8ec81d8f429
Donnie Koshy <donnie.koshy@gmail.com>
2020-06-02 06:47:39 -0700

    Updated Exercise#1

--- week2ex1.py
+++ week2ex1.py
@@ -1,5 +1,5 @@
 #!usr/bin/env python
-from netmiko import ConnectHandler,file_transfer
+from netmiko import ConnectHandler
 from getpass import getpass
 
 router={
@@ -10,13 +10,17 @@ router={
 }
 net_connect=ConnectHandler(**router)
 device=net_connect.find_prompt().rstrip('#')
-print("\n Logged into "+device)
-net_connect.send_command_timing('ping')
-net_connect.send_command_timing('\n')
-net_connect.send_command('8.8.8.8')
-output=net_connect.send_command_timing("\n")
+print("\n Logged into "+device+"\n")
+output=net_connect.send_command('ping',expect_string=r'ip',strip_command=False,strip_prompt=False)
+output+=net_connect.send_command('\n',expect_string=r'address',strip_command=False)
+output+=net_connect.send_command('8.8.8.8',expect_string=r'count',strip_command=False)
+output+=net_connect.send_command("\n",expect_string=r'size',strip_command=False)
+output+=net_connect.send_command("\n",expect_string=r'seconds',strip_command=False)
+output+=net_connect.send_command("\n",expect_string=r'commands',strip_command=False)
+output+=net_connect.send_command("\n",expect_string=r'sizes',strip_command=False)
+output+=net_connect.send_command("\n",expect_string=r'#',strip_command=False)
+
 print(output)
-print(net_connect.find_prompt())
 
 net_connect.disconnect()
 
